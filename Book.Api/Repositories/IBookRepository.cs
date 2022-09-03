471be117beb34419b68aa6188a496a1ccafe1d02
Klauss Rodrigo Diorio <krdiorio@outlook.com>
2022-09-03 17:00:12 -0300

    Aula 1

---
+++ Book.Api/Repositories/IBookRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using BooksApi.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace BooksApi.Repositories
+{
+    public interface IBookRepository
+    {
+
+        Task<IEnumerable<Book>> GetAll();
+
+        Task<Book> Get(int id);
+
+        Task Update();
+
+        Task Delete(int id);
+
+
+    }
+}
--- Book.Api/Startup.cs
+++ Book.Api/Startup.cs
@@ -1,7 +1,9 @@
+using BooksApi.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,7 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Book.Api
+namespace BooksApi
 {
     public class Startup
     {
@@ -26,11 +28,11 @@ namespace Book.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<BookContext>(x => x.UseSqlite("Data source=books.dbo"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Book.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooksApi", Version = "v1" });
             });
         }
 
@@ -41,7 +43,7 @@ namespace Book.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BooksApi v1"));
             }
 
             app.UseHttpsRedirection();
